5c9b71e12b1632722f4b62e56c202df7aa8e0133
Alex Jercan <a.jercan@accenture.com>
2022-04-26 11:08:50 +0300

    added day 19

--- pkg/day19/day19.go
+++ pkg/day19/day19.go
@@ -1,5 +1,44 @@
 package day19
 
+import (
+	"strconv"
+	"strings"
+)
+
+type Node struct {
+	Value int
+	Next  *Node
+}
+
+func solve1(n int) int {
+	elf := &Node{Value: 1}
+	it := elf
+	for i := 2; i <= n; i++ {
+		it.Next = &Node{Value: i}
+		it = it.Next
+	}
+	it.Next = elf
+
+	for elf.Next != elf {
+		elf.Next = elf.Next.Next
+		elf = elf.Next
+	}
+
+	return elf.Value
+}
+
+func solve2(n int) int {
+	i := 1
+
+	for i*3 < n {
+		i *= 3
+	}
+
+	return (n - i)
+}
+
 func Solve(input string) string {
-	return "Day19\nPart1: " + "0" + "\nPart2: " + "0"
+	n, _ := strconv.Atoi(strings.TrimSpace(input))
+
+	return "Day19\nPart1: " + strconv.Itoa(solve1(n)) + "\nPart2: " + strconv.Itoa(solve2(n))
 }
